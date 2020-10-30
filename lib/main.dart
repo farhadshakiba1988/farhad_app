5f8e06f9eb7056202dde98522888ef77bbaf8a5d
Python <farhadshakiba@yahoo.com>
2020-10-30 23:34:10 +0330

    first code

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:farhad_app/screens/home_screens.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: HomeScreen(),
+));
+
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold();
+  }
+}
+
+
+
